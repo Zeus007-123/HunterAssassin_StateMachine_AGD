acdbc7eb72baaafbaf7ed38ca5a549359a6f3892
AKSHAY R <akshay.renjith.1@gmail.com>
2025-02-25 21:56:05 +0530

    Created an Enemy State Machine
    - Created an IState interface with following methods to be implemented:
    
    OnStateEnter()
    Update()
    OnStateExit()
    
    - Created an enum called OnePunchManStates and define various states for One Punch Man
    
    - Created a StateMachine class called OnePunchManStateMachine responsible for managing all state transitions.
    - Initialized all states inside OnePunchManStateMachine and map them to their corresponding enum value.
    - Setting the Owner of this State Machine as well as all the states that were created.
    - Maintained the current state of enemy and update it each frame.
    - Created a ChangeState(IState newState) method which will switch the current state to new state.
    
    - Implemented concrete state classes for different enemy behavior:
    
    1. Idle State => Enemy does nothing for a few seconds and then transitions to rotating state
    2. Rotating State => Rotates 180 degrees and goes back into Idl state
    3. Shooting State => If the player enters enemy range, enemy starts shooting towards player. If player goes out of enemy range, enemy transitions back to Idle State.

---
+++ Assets/Scripts/Enemy/States/IState.cs
@@ -0,0 +1,11 @@
+namespace StatePattern.Enemy
+{
+    public interface IState
+    {
+        public OnePunchManController Owner { get; set; }
+        public void OnStateEnter();
+        public void Update();
+        public void OnStateExit();
+    }
+
+}
